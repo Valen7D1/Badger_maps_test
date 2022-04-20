6614f808e4e4232e16c6f5a5e4ec1fc3be31da1d
yorentior1 <100429028@alumnos.uc3m.es>
2022-04-20 19:51:16 +0200

    Oldest check added to main

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This is a test from Badger Maps
 
 We will consider that the read of the csv wont cause problems
+
+I will consider that when asked for "customer" you mean the whole row, and so I will return
+
+I will also consider that when errors handled, is only needed a print and not the full proper error message
--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 import pandas as pd
+import time
+
+
+def oldest_check(dataset):
+    # modify index for visual simplicity
+    winner = None
+    for i in range(dataset.shape[0]):
+        data = dataset.iloc[[i]].values[0]
+        if type(data[6]) != float:
+            if winner is None:
+                try:
+                    time.strptime(data[6], "%d/%m/%Y")
+                    winner = data
+                except ValueError:
+                    print("ERROR: Invalid date format, ignoring row", str(i))
+            else:
+                date1 = time.strptime(winner[6], "%d/%m/%Y")
+                try:
+                    date2 = time.strptime(data[6], "%d/%m/%Y")
+                    if date1 > date2:
+                        # draws will let the previous winner to stay
+                        winner = data
+                except ValueError:
+                    print("ERROR: Invalid date format, ignoring row", str(i))
+
+    return winner
+
 
 if __name__ == "__main__":
     # first we need to read the csv file, we will be using pandas for the task
@@ -10,3 +37,8 @@ if __name__ == "__main__":
     # if size is different there are empty rows
     if df.shape[0] != actual_size:
         print("ERROR: There were", str(actual_size - df.shape[0]), "columns empty")
+
+    # if used the exercise will be simplified to almost nothing
+    # search for the most old check-in
+    elder = oldest_check(df)
+    print("Oldest customer check-in:", list(elder))
