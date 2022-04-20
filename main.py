acbba3ab5df37126f8cc3b75f279e038c884e676
yorentior1 <100429028@alumnos.uc3m.es>
2022-04-20 19:54:13 +0200

    Latest check added to main (almost the same function)

--- main.py
+++ main.py
@@ -27,14 +27,41 @@ def oldest_check(dataset):
     return winner
 
 
+def latest_check(dataset):
+    # modify index for visual simplicity
+    winner = None
+    for i in range(dataset.shape[0]):
+        data = dataset.iloc[[i]].values[0]
+        if type(data[6]) != float:
+            if winner is None:
+                try:
+                    time.strptime(data[6], "%d/%m/%Y")
+                    winner = data
+                except ValueError:
+                    print("ERROR: Invalid date format, ignoring row ", str(i))
+            else:
+                date1 = time.strptime(winner[6], "%d/%m/%Y")
+                try:
+                    date2 = time.strptime(data[6], "%d/%m/%Y")
+                    if date1 < date2:
+                        # draws will let the previous winner to stay
+                        winner = data
+                except ValueError:
+                    print("ERROR: Invalid date format, ignoring row ", str(i))
+
+    return winner
+
+
 if __name__ == "__main__":
     # first we need to read the csv file, we will be using pandas for the task
     df = pd.read_csv("Sample test file - Sheet1.csv")
+
     # store actual size for later error detection
     actual_size = df.shape[0]
     # delete al rows with NaN in all their columns
     df = df.dropna(how='all')
     # if size is different there are empty rows
+
     if df.shape[0] != actual_size:
         print("ERROR: There were", str(actual_size - df.shape[0]), "columns empty")
 
@@ -42,3 +69,7 @@ if __name__ == "__main__":
     # search for the most old check-in
     elder = oldest_check(df)
     print("Oldest customer check-in:", list(elder))
+
+    # search for the most recent check-in
+    youngling = latest_check(df)
+    print("\nLatest customer check-in:", list(youngling))
